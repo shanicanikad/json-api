ac45eaf63fa8407244ba32c5473fda4097781d58
SHANICA_DENIS <shanicanikad@gmail.com>
2020-11-06 13:28:48 -0500

    nre

--- db/connections.js
+++ db/connections.js
@@ -4,6 +4,6 @@ let mongoURL = "";
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
   } else {
-    mongoURI = "mongodb://localhost/Naruto";
+    mongoURI = "mongodb://localhost/naruto";
   }
 module.exports = mongoose;
